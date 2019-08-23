e23ec409d37e095a229d16204ca8532c10a17248
Di Wen <ifyouseewendy@gmail.com>
2019-08-23 09:31:11 -0400

    Suppoprt passing [Int]

--- mruby-sys/vendor/mruby-bc7c5d3/entry.c
+++ mruby-sys/vendor/mruby-bc7c5d3/entry.c
@@ -1,44 +1,48 @@
+#include <stdlib.h>
 #include <mruby.h>
 #include <mruby/compile.h>
-#include <mruby/string.h>
+#include <mruby/variable.h>
 
 #define WASM_EXPORT __attribute__((visibility("default")))
 
-/*  */
-/* struct sliceint { */
-/*     int *data; */
-/*     unsigned length; */
-/* } */
-
-struct SliceUtf8 {
-    char *data;
-    unsigned length;
+struct slice_of_int {
+    int32_t *data;
+    uint32_t length;
 };
 
-WASM_EXPORT struct SliceUtf8* run(struct SliceUtf8* s) {
-	mrb_state *mrb = mrb_open();
-	if (!mrb) { abort(); }
+mrb_value slice_of_int_each(mrb_state *mrb, mrb_value self) {
+    mrb_value block;
+    mrb_get_args(mrb, "&!", &block);
 
-        // Load function
-	mrb_value func = mrb_load_string(mrb, "def run(str); \"hello #{str}\"; end");
-        mrb_sym sym = mrb_intern_lit(mrb, "run"); // Symbol for method.
+    struct slice_of_int *slice = mrb_cptr(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@struct_ptr")));
 
-        // Prepare argument
-        mrb_value str = mrb_str_new_static(mrb, s->data, s->length);
+    for(uint32_t i = 0; i < slice->length; i++) {
+        mrb_yield(mrb, block, mrb_fixnum_value(slice->data[i]));
+    }
 
-        // Call function and prepare return
-        mrb_value ret_str = mrb_funcall_argv(mrb, func, sym, 1, &str);
+    return self;
+}
 
-        struct SliceUtf8 slice = {
-            .data = RSTRING_PTR(ret_str),
-            .length = RSTRING_LEN(ret_str)
-        };
+mrb_value slice_of_int_length(mrb_state *mrb, mrb_value self) {
+    struct slice_of_int *slice = mrb_cptr(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@struct_ptr")));
+    return mrb_fixnum_value(slice->length);
+}
 
-	mrb_close(mrb);
+WASM_EXPORT int run(struct slice_of_int *slice) {
+    mrb_state *mrb = mrb_open();
+    if (!mrb) { abort(); }
 
-        return &slice;
+    mrb_value top = mrb_load_string(mrb, "class SliceOfInt; include Enumerable; def initialize(struct_ptr); @struct_ptr = struct_ptr; end; end; def run(slice); slice.reduce(:+); end");
+    struct RClass *mrb_slice_of_int = mrb_class_get(mrb, "SliceOfInt");
+    mrb_define_method(mrb, mrb_slice_of_int, "each", &slice_of_int_each, MRB_ARGS_NONE());
+    mrb_define_method(mrb, mrb_slice_of_int, "length", &slice_of_int_length, MRB_ARGS_NONE());
+
+    mrb_value arg = slice ? mrb_obj_new(mrb, mrb_slice_of_int, 1, (mrb_value[]){ mrb_cptr_value(mrb, slice) }) : mrb_nil_value();
+    mrb_value rv = mrb_funcall_argv(mrb, top, mrb_intern_lit(mrb, "run"), 1, &arg);
+    mrb_close(mrb);
+    return mrb_fixnum(rv);
 }
 
 WASM_EXPORT void* shopify_runtime_allocate(size_t s) {
-	return malloc(s);
+    return malloc(s);
 }
