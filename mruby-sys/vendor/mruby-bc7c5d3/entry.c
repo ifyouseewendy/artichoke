1f846eaddfb8da405b0cd3c29ca7c02eb8c75561
Di Wen <ifyouseewendy@gmail.com>
2019-08-27 09:28:04 -0400

    Return struct with int field for real*
    The return struct has to be heap allocated

--- mruby-sys/vendor/mruby-bc7c5d3/entry.c
+++ mruby-sys/vendor/mruby-bc7c5d3/entry.c
@@ -5,28 +5,20 @@
 
 #define WASM_EXPORT __attribute__((visibility("default")))
 
-/* struct Input { */
-/*     int price; */
-/* }; */
-/*  */
-/* mrb_value input_price(mrb_state *mrb, mrb_value self) { */
-/*     struct Input *ptr = mrb_cptr(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@struct_ptr"))); */
-/*     return mrb_fixnum_value(ptr->price); */
-/* } */
+struct Input {
+    int price;
+};
+
+mrb_value input_price(mrb_state *mrb, mrb_value self) {
+    struct Input *ptr = mrb_cptr(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@struct_ptr")));
+    return mrb_fixnum_value(ptr->price);
+}
 
 struct Output {
     int price;
 };
-mrb_value output_price(mrb_state *mrb, mrb_value self) {
-    struct Output *ptr = mrb_cptr(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@price")));
-    return mrb_fixnum_value(ptr->price);
-}
-
-/* mrb_value output_new(mrb_state *mrb, mrb_value self) { */
-/*     mrb_int i = 42; */
-/*     mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@price"), mrb_fixnum_value(i)); */
-/*  */
-/*     return self; */
+/* mrb_value output_price(mrb_state *mrb, mrb_value self) { */
+/*     return mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@price")); */
 /* } */
 
 /* struct slice_of_int { */
@@ -52,19 +44,42 @@ mrb_value output_price(mrb_state *mrb, mrb_value self) {
 /*     return mrb_fixnum_value(slice->length); */
 /* } */
 
-WASM_EXPORT int run(int price) {
+WASM_EXPORT struct Output* run(struct Input* input) {
     mrb_state *mrb = mrb_open();
     if (!mrb) { abort(); }
 
-    mrb_value script = mrb_load_string(mrb, "class Output; def price; @price; end; def initialize; @price = 42; end; end; def run(num); Output.new; end");
-    /* struct RClass *mrb_output = mrb_class_get(mrb, "Output"); */
-    /* mrb_define_method(mrb, mrb_output, "price", &output_price, MRB_ARGS_NONE()); */
+    char* ruby_script = "\
+                            class Input \n\
+                              def initialize(struct_ptr) \n\
+                                @struct_ptr = struct_ptr \n\
+                              end \n\
+                            end \n\
+                            class Output \n\
+                              def initialize(price) \n\
+                                @price = price \n\
+                              end \n\
+                            end \n\
+                            def run(input) \n\
+                              Output.new(input.price) \n\
+                            end \n\
+                        ";
+    mrb_value script = mrb_load_string(mrb, ruby_script);
 
-    mrb_int i = 42;
-    mrb_value p = mrb_fixnum_value(i);
-    mrb_value rv = mrb_funcall_argv(mrb, script, mrb_intern_lit(mrb, "run"), 1, &p);
+    struct RClass *mrb_input = mrb_class_get(mrb, "Input");
+    mrb_define_method(mrb, mrb_input, "price", &input_price, MRB_ARGS_NONE());
 
-    return mrb_fixnum(mrb_iv_get(mrb, rv, mrb_intern_lit(mrb, "@price")));
+    mrb_value v = mrb_cptr_value(mrb, input);
+    mrb_value arg = input ? mrb_obj_new(mrb, mrb_input, 1, &v) : mrb_nil_value();
+    mrb_value rv = mrb_funcall_argv(mrb, script, mrb_intern_lit(mrb, "run"), 1, &arg);
+
+    mrb_value price = mrb_iv_get(mrb, rv, mrb_intern_lit(mrb, "@price"));
+
+    struct Output *ret = malloc(sizeof(struct Output));
+    ret->price = mrb_int(mrb, price);
+
+    mrb_close(mrb);
+
+    return ret;
 }
 
 WASM_EXPORT void* shopify_runtime_allocate(size_t s) {
